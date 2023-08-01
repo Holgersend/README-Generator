833bcf3e7a28683f06825172b9d4189a825c2f80
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-07-31 21:13:28 -0500

    edited index.js
    created how many user input the application will have.

---
+++ index.js
@@ -0,0 +1,42 @@
+const inquirer = inquirer("inquirer");
+const fs = require("fs");
+
+    inquirer
+        .prompt([
+        {
+            type: "input",
+            name: "title",
+            message:"Enter your proyect title:",
+        }
+        {
+            type: "input",
+            name: "title",
+            message:"Enter your proyect title:",
+        }
+        {
+            type: "input",
+            name: "title",
+            message:"Enter your proyect title:",
+        }
+        {
+            type: "input",
+            name: "title",
+            message:"Enter your proyect title:",
+        }
+        {
+            type: "input",
+            name: "title",
+            message:"Enter your proyect title:",
+        }
+        {
+            type: "input",
+            name: "title",
+            message:"Enter your proyect title:",
+        }
+        {
+            type: "input",
+            name: "title",
+            message:"Enter your proyect title:",
+        }
+        ])
+        
